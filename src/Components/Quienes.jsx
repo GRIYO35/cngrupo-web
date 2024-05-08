e4db1931215432263bf9ac7979ee3c4a3f79a0f3
GRIYO35 <marcos_albarado@hotmail.com>
2024-05-08 17:06:41 -0300

    textos Ingles Quienes

--- src/Components/Quienes.jsx
+++ src/Components/Quienes.jsx
@@ -7,7 +7,7 @@ import fotoellos2 from '../Assets/TOMI.png';
 import fotoellos5 from '../Assets/261x353 TOMI.png';
 import fotoellos3 from '../Assets/EDU solo.png';
 import fotoellos6 from '../Assets/261x353 EDU h.png';
-/*import { useTranslation } from "react-i18next";*/
+import { useTranslation } from "react-i18next";
 import gerentes from '../Assets/gerentes.png';
 import gerentes1 from '../Assets/479x279 GERENTES.png';
 import NAVBAR from "./NavBar"
@@ -15,8 +15,9 @@ import "./Quienes.css"
 
 
 const Quienes = () => {
-  /*const [t, i18next] = useTranslation("global");
-  const [isOpen, setIsOpen] = useState(false);*/
+  // eslint-disable-next-line no-unused-vars
+  const [t, i18next] = useTranslation("global");
+  /*const [isOpen, setIsOpen] = useState(false);*/
   const [currentImage, setCurrentImage] = useState(gerentes);
   const [currentImage1, setCurrentImage1] = useState(fotoellos1);
   const [currentImage2, setCurrentImage2] = useState(fotoellos2);
@@ -62,24 +63,23 @@ const Quienes = () => {
       <div className="txt-redesQuienes">
            <div className="txt-container-empresaQuienes">
                <div className="linea-titulo">
-                   <hr class="mi-lineaQuienes"></hr><p className="nuestra-empresa">NUESTRA EMPRESA</p>
+                   <hr class="mi-lineaQuienes"></hr><p className="nuestra-empresa">{t("li.OUR COMPANY")}</p>
                </div>
-               <h2 className="Quienes-title">QUIÉNES SOMOS</h2>
+               <h2 className={i18next.language === "es" ? "Quienes-title" : "Quienes-titleEN"}>{t("quienes.about us")}</h2>
            </div>
       </div>
       </div>
       <div className="txt-redes-quienes">
           <div className="txt-container-quienes">
-             <h2 className="quienes-info-txt"><span className="txt-empresa-spanQuienes">CN GRUPO </span>
-             nace como una empresa familiar y cuenta con más <span className="txt-empresa-spanQuienes"> 35 años </span> 
-             de trayectoria, mas de <span className="txt-empresa-spanQuienes"> 250 colaboradores </span> y 
-             <span className="txt-empresa-spanQuienes"> 6 bases operativas </span> 
-             distribuidas en puntos estratégicos de Salta y norte de Chile. 
+             <h2 className="quienes-info-txt"><span className="txt-empresa-spanQuienes">{t("txtQuines.txt1")} </span>
+             {t("txtQuines.txt2")} <span className="txt-empresa-spanQuienes"> {t("txtQuines.txt3")} </span> 
+             {t("txtQuines.txt4")} <span className="txt-empresa-spanQuienes"> {t("txtQuines.txt5")} </span> 
+             {t("txtQuines.txt6")} 
              <br></br>
              <br></br>
-             En el transcurso de nuestro desarrollo empresario hemos ido diversificando negocios, apostando siempre a <label htmlFor=""></label>
-             <span className="txt-empresa-spanQuienes"> innovación</span>, al <span className="txt-empresa-spanQuienes"> desarrollo profesional </span> 
-             de nuestros equipos y a las relaciones estratégicas con nuestros proveedores, clientes y comunidad.
+             {t("txtQuines.txt7")} <label htmlFor=""></label>
+             <span className="txt-empresa-spanQuienes"> {t("txtQuines.txt8")}</span> 
+             {t("txtQuines.txt9")}
              </h2>
           </div>
           <div className="gerentes">
@@ -89,23 +89,23 @@ const Quienes = () => {
 
             <div className='fotosdellos'>
                  <div className='container-fotosdellos'>
-                    <div className='foto-txt-quienes'><img src={currentImage1} alt="" className='fotoellos'/> <h2 className="quienes-title-foto">EDUARDO GÓMEZ NAAR</h2><p className="p-quienes1">Fundador y CEO de CN Grupo</p>   
+                    <div className='foto-txt-quienes'><img src={currentImage1} alt="" className='fotoellos'/> <h2 className="quienes-title-foto">EDUARDO GÓMEZ NAAR</h2><p className="p-quienes1">{t("txtDirectorio.txt1")}</p>   
                       <hr className="linea-quienes2"></hr>
                     </div>
-                    <div className='foto-txt-quienes'><img src={currentImage2} alt="" className='fotoellos'/> <h2 className="quienes-title-foto">TOMÁS GÓMEZ NAAR</h2><p className="p-quienes1">Director de Negocios</p> 
+                    <div className='foto-txt-quienes'><img src={currentImage2} alt="" className='fotoellos'/> <h2 className="quienes-title-foto">TOMÁS GÓMEZ NAAR</h2><p className="p-quienes1">{t("txtDirectorio.txt2")}</p> 
                       <hr className="linea-quienes2"></hr>
                     </div>
-                    <div className='foto-txt-quienes'><img src={currentImage3} alt="" className='fotoellos'/> <h2 className="quienes-title-foto">EDUARDO GÓMEZ NAAR</h2><p className="p-quienes1">Director de Áreas Staff</p>  
+                    <div className='foto-txt-quienes'><img src={currentImage3} alt="" className='fotoellos'/> <h2 className="quienes-title-foto">EDUARDO GÓMEZ NAAR</h2><p className="p-quienes1">{t("txtDirectorio.txt3")}</p>  
                       <hr className="linea-quienes2"></hr>
                     </div>
                  </div>
              </div>
             <div className="footer">
           <div className="txt1">
-            <p className="txt-foo">Av. Chile 1275, A4400 Salta</p>
+            <p className="txt-foo">CRECER & HACER CRECER</p>
           </div>
           <div className="txt2">
-            <p className="txt-foo">© 2024 CN Grupo - Todos los derechos reservados</p>
+            <p className="txt-foo">{t("txt-foo2.Footer")}</p>
           </div>
         </div>
     </div>
